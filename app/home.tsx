e45ac142457e346a69fa1bc7823711d516a99881
Ahsan Javed <jahsan850@gmail.com>
2025-05-30 22:12:49 +0200

    tablayout

---
+++ app/home.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "react";
+import { Text, View } from "react-native";
+
+export default function HomeScreen(): JSX.Element {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
